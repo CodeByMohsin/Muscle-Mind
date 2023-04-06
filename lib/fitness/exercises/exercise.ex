87b3d33e075ed0045d663985d832dea481444774
0xmohsinpathan <0xmohsinpathan@gmail.com>
2023-04-06 19:13:32 +0530

    ✨ New Feature:  add schema for exercise
    mix phx.gen.live Exercises Exercise exercises name:string description:text gif_url:string level:string  type:string

---
+++ lib/fitness/exercises/exercise.ex
@@ -0,0 +1,21 @@
+defmodule Fitness.Exercises.Exercise do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "exercises" do
+    field :description, :string
+    field :gif_url, :string
+    field :level, :string
+    field :name, :string
+    field :type, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(exercise, attrs) do
+    exercise
+    |> cast(attrs, [:name, :description, :gif_url, :level, :type])
+    |> validate_required([:name, :description, :gif_url, :level, :type])
+  end
+end
---
+++ test/fitness/exercises_test.exs
@@ -0,0 +1,67 @@
+defmodule Fitness.ExercisesTest do
+  use Fitness.DataCase
+
+  alias Fitness.Exercises
+
+  describe "exercises" do
+    alias Fitness.Exercises.Exercise
+
+    import Fitness.ExercisesFixtures
+
+    @invalid_attrs %{description: nil, gif_url: nil, level: nil, name: nil, type: nil}
+
+    test "list_exercises/0 returns all exercises" do
+      exercise = exercise_fixture()
+      assert Exercises.list_exercises() == [exercise]
+    end
+
+    test "get_exercise!/1 returns the exercise with given id" do
+      exercise = exercise_fixture()
+      assert Exercises.get_exercise!(exercise.id) == exercise
+    end
+
+    test "create_exercise/1 with valid data creates a exercise" do
+      valid_attrs = %{description: "some description", gif_url: "some gif_url", level: "some level", name: "some name", type: "some type"}
+
+      assert {:ok, %Exercise{} = exercise} = Exercises.create_exercise(valid_attrs)
+      assert exercise.description == "some description"
+      assert exercise.gif_url == "some gif_url"
+      assert exercise.level == "some level"
+      assert exercise.name == "some name"
+      assert exercise.type == "some type"
+    end
+
+    test "create_exercise/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Exercises.create_exercise(@invalid_attrs)
+    end
+
+    test "update_exercise/2 with valid data updates the exercise" do
+      exercise = exercise_fixture()
+      update_attrs = %{description: "some updated description", gif_url: "some updated gif_url", level: "some updated level", name: "some updated name", type: "some updated type"}
+
+      assert {:ok, %Exercise{} = exercise} = Exercises.update_exercise(exercise, update_attrs)
+      assert exercise.description == "some updated description"
+      assert exercise.gif_url == "some updated gif_url"
+      assert exercise.level == "some updated level"
+      assert exercise.name == "some updated name"
+      assert exercise.type == "some updated type"
+    end
+
+    test "update_exercise/2 with invalid data returns error changeset" do
+      exercise = exercise_fixture()
+      assert {:error, %Ecto.Changeset{}} = Exercises.update_exercise(exercise, @invalid_attrs)
+      assert exercise == Exercises.get_exercise!(exercise.id)
+    end
+
+    test "delete_exercise/1 deletes the exercise" do
+      exercise = exercise_fixture()
+      assert {:ok, %Exercise{}} = Exercises.delete_exercise(exercise)
+      assert_raise Ecto.NoResultsError, fn -> Exercises.get_exercise!(exercise.id) end
+    end
+
+    test "change_exercise/1 returns a exercise changeset" do
+      exercise = exercise_fixture()
+      assert %Ecto.Changeset{} = Exercises.change_exercise(exercise)
+    end
+  end
+end
