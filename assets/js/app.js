62cf86c3f3c7ac8dfbd15449c0ba122b628808da
Maqbool <maqbool@maqbool.net>
2023-08-23 15:42:58 +0530

    add easy mde

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,16 +21,53 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+
+import easyMDE from "easyMDE";
+
+let Hooks = {}
+
+
+Hooks.easyMDE = {
+    mounted() {
+
+        const editorInstance = new easyMDE({
+            element: this.el,
+            forceSync: true,
+            initialValue: "Hello world!"
+        })
+
+
+        editorInstance.codemirror.on("change", () => {
+            console.log("changed")
+            this.pushEventTo(
+                this.el,
+                "change",
+                { richtext_data: editorInstance.value() }
+            )
+        })
+        let button = document.getElementById("easy_text_editor_submit")
+
+        let container = document.getElementsByClassName("EasyMDEContainer")[0]
+        container.appendChild(button)
+    },
+    updated() {
+
+        console.log("updated")
+
+
+    }
+}
+
 
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks, params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
--- lib/fitness_web/live/chat_live/chat_live.ex
+++ lib/fitness_web/live/chat_live/chat_live.ex
@@ -17,8 +17,6 @@ defmodule FitnessWeb.ChatLive do
 
     default_room = Repo.get(Fitness.Chats.Schema.Room, "8c30bb29-1aa2-4af2-8091-70032c1a9ffa")
 
-
-
     socket =
       socket
       |> assign(changeset: changeset)
@@ -31,6 +29,12 @@ defmodule FitnessWeb.ChatLive do
 
   @impl true
   def handle_event("validate", %{"message" => params}, socket) do
+    IO.inspect(params)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("change", params, socket) do
     IO.inspect(params)
     {:noreply, socket}
   end
@@ -58,16 +62,47 @@ defmodule FitnessWeb.ChatLive do
     ~H"""
     <h1>Room: <%= @room.name %></h1>
 
-    <div :for={message <- @messages} id="messages_card" phx-update="stream">
+    <div :for={message <- @messages} id={message.id}>
       <%= message.data %>
       <%= message.inserted_at %>
       <%= message.user.username %>
     </div>
 
-    <.form :let={f} for={@changeset} phx-change="validate" phx-submit="create">
-      <%= text_input(f, :data, class: "w-max") %>
-      <button class="block" type="create">Save</button>
-    </.form>
+    <div class=" relative mt-10">
+      <.form
+        :let={f}
+        for={@changeset}
+        phx-submit="create"
+        id="easy_text_editor_form"
+        phx-validate="validate"
+      >
+        <div id="rich-text-editor">
+          <%= textarea(f, :data,
+            id: "rich_text_input",
+            phx_hook: "easyMDE",
+            phx_update: "ignore",
+            class: "hidden"
+          ) %>
+        </div>
+
+        <button type="submit" id="easy_text_editor_submit" class="absolute right-1 bottom-10 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+            />
+          </svg>
+        </button>
+      </.form>
+    </div>
     """
   end
 end
